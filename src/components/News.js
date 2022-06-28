50f1e79216a6602a7351afd2339d73dfbaa8157e
Ankit Mishra <ankitm17.2001@gmail.com>
2022-06-29 02:49:24 +0530

    news Monkey started adding navbar component

---
+++ src/components/News.js
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export class News extends Component {
+ 
+
+  render() {
+    return (
+      <div>
+        This is a news component.
+        <NewsItem/>
+        <NewsItem/>
+        <NewsItem/>
+        <NewsItem/>
+        <NewsItem/>
+        <NewsItem/>
+      </div>
+    )
+  }
+}
+
+export default News
